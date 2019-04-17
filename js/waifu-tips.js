1d1b9cf7fb3da4611150fb9f4190b8c3f852b318
jhx520 <37178161+jhx520@users.noreply.github.com>
2019-04-17 13:53:08 +0800

    1

--- js/waifu-tips.js
+++ js/waifu-tips.js
@@ -52,7 +52,7 @@ live2d_settings['showCopyMessage']      = true;         // 显示 复制内容 �
 live2d_settings['showWelcomeMessage']   = true;         // 显示进入面页欢迎词
 
 //看板娘样式设置
-live2d_settings['waifuSize']            = '120x110';    // 看板娘大小，例如 '280x250', '600x535'
+live2d_settings['waifuSize']            = '130x100';    // 看板娘大小，例如 '280x250', '600x535'
 live2d_settings['waifuTipsSize']        = '250x70';     // 提示框大小，例如 '250x70', '570x150'
 live2d_settings['waifuFontSize']        = '12px';       // 提示框字体，例如 '12px', '30px'
 live2d_settings['waifuToolFont']        = '14px';       // 工具栏字体，例如 '14px', '36px'
